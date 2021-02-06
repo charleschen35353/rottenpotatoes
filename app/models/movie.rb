a48ab08183eedc5f613f1f14687fd625f9e8fe7a
Liang-yu Chen <lc3533@columbia.edu>
2021-02-06 17:32:59 +0000

    checkbox added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
-end
+  @@all_ratings = ['G','PG','PG-13','R']
+
+  def self.all_ratings
+    @@all_ratings 
+  end
+  
+  def self.with_ratings(ratings_list)
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+  #  movies with those ratings
+  # if ratings_list is nil, retrieve ALL movies
+    ratings_list = @@all_ratings if ratings_list.size == 0
+    return self.where(rating: ratings_list)
+  end
+  
+end 
